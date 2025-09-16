9f4f48d0cb5ef4e7f789d63cbe42eb57f17daf71
iyui <26142848@qq.com>
2025-09-16 21:19:44 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 2614,
+  name: 'iYui-个人订阅',
   version: 0,
-  author: 'author',
+  author: '现阶段测试，不定时更新r',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/26142848/GKD_List_Personal/issues',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
