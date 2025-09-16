4cc416c2127bba881b4c309d865a39cc021debec
iyui <26142848@qq.com>
2025-09-16 21:20:25 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,5 +1,4 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
-import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
 import globalGroups from './globalGroups';
 
