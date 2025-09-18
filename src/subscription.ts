ba31caf16901e221ccfcf1682fafb58426ee505b
iyui <26142848@qq.com>
2025-09-19 03:47:34 +0800

    update

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,16 +1,28 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
-import categories from './categories';
-import globalGroups from './globalGroups';
 import { batchImportApps } from '@gkd-kit/tools';
+import categories from './categories';
+import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+const rawApps: RawApp[] = [];
+apps.forEach((appConfig) => {
+  appConfig.groups?.forEach((g: RawAppGroup) => {
+    if (g.name.startsWith('开屏广告')) {
+      g.order = OPEN_AD_ORDER;
+    }
+  });
+  rawApps.push(appConfig);
+});
 
 export default defineGkdSubscription({
   id: 2614,
   name: 'iYui-个人订阅',
   version: 0,
-  author: '现阶段测试，不定时更新r',
+  author: 'iYui',
   checkUpdateUrl: './gkd.version.json5',
   supportUri: 'https://github.com/26142848/GKD_List_Personal/issues',
   categories,
   globalGroups,
-  apps: await batchImportApps(`${import.meta.dirname}/apps`),
+  apps: rawApps,
 });
